b4003549bde251f3ce7b12a9f58654ae4170328c
Andrew Graves <agraves@codecademy.com>
2023-05-04 13:56:05 -0400

    add openai message

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+//const github = require("@actions/github");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const fetch = async () => {
+  // openai
+  const apiKey = core.getInput("OPEN_AI_API_KEY");
+  const configuration = new Configuration({
+    apiKey: apiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Tell me a knock knock joke.",
+    temperature: 0,
+    max_tokens: 1000,
+  });
+  const { message } = response.data.choices[0];
+  return message;
+};
 
 try {
-  // `who-to-greet` input defined in action metadata file
+  // file paths
   const filePaths = core.getInput("filepaths");
-  const message = `Filepaths: ${filePaths}!`;
-  console.log(message);
+  const filePathsMessage = `filepaths: ${filePaths}`;
+  console.log(filePathsMessage);
+  core.setOutput("filepaths", filePaths);
+  const message = fetch();
   core.setOutput("message", message);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
