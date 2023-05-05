101570114c07511c9a4138d2a18bb7daf72485b6
Andrew Graves <agraves@codecademy.com>
2023-05-05 07:55:57 -0400

    update prompt

--- dist/index.js
+++ dist/index.js
@@ -9900,10 +9900,10 @@ const { Configuration, OpenAIApi } = __nccwpck_require__(3485);
 
 async function askOpenAI(fileContent) {
   const prompt = `
-	List all the ways that this file breaks the following rules:
+	List all the ways that this markdown file breaks the following rules:
 
 	Rules:
-	1. Do not use any h1 headers (in Markdown preceded by single #).
+	1. Do not use any markdown h1 headers.
 	2. Markdown headers should be written in Title Case, which means all words in the header are capitalized. 
 	3. Markdown headers should not exceed 5 words.
 	4. Markdown headers should not include ending punctuation.
